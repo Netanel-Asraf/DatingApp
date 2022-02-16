00d9549e75b9f71af5ecdc04e4675b153afd0d8c
Netanel Asraf <netanel9530@gmail.com>
2022-02-16 15:08:30 +0200

    End of section 6

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { ToastrModule } from 'ngx-toastr';
+import { SharedModule } from './_models/shared.module';
 
 
 @NgModule({
@@ -25,7 +27,7 @@ import { RegisterComponent } from './register/register.component';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
